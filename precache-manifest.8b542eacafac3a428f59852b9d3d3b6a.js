f387e88be7c2e31452f4f4ad094da5d600e89d67
newlife-nbd <pengpengw@hotmail.com>
2020-10-16 19:03:56 -0400

    Updates

---
+++ precache-manifest.8b542eacafac3a428f59852b9d3d3b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63157f7583be551efcc577832c730550",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1172b0b738c09f1c79ea",
+    "url": "/monsters-rolodex/static/css/main.338c9142.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1172b0b738c09f1c79ea",
+    "url": "/monsters-rolodex/static/js/main.67ccff9c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
